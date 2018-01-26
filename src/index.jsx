ce914ccd1a5d999a9f753c29fb8f420b1da4f20b
Christopher Kobler <c.kobler@gmx.net>
2018-01-26 15:38:02 +0100

    Add and update components

---
+++ src/index.jsx
@@ -0,0 +1,53 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+
+const API_KEY = "AIzaSyAbXhjYckmesIaTIlmANBjb_yz4tmj7fdk";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [], selectedVideo: null };
+
+    this.searchVideo("reactjs");
+  }
+
+  searchVideo(term) {
+    YTSearch(
+      {
+        key: API_KEY,
+        term: term
+      },
+      videos => {
+        this.setState({ videos: videos, selectedVideo: videos[0] });
+      }
+    );
+  }
+
+  render() {
+    const searchVideo = _.debounce(term => {
+      this.searchVideo(term);
+    }, 300);
+
+    return (
+      <div>
+        <SearchBar onSearchTermChange={searchVideo} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => {
+            this.setState({ selectedVideo });
+          }}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
